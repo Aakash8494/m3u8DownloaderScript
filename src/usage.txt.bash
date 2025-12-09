e24351ca986db944db3056e4643907f3f28b8603
Aakash Jadhav <72457949+Aakash8494@users.noreply.github.com>
2025-12-09 16:13:09 +0530

    usage txt

--- src/usage.txt.bash
+++ src/usage.txt.bash
@@ -1,29 +1,49 @@
 # dont forget to add {vid}
 python downloader.py template \
-  --template "https://video.acharyaprashant.org/dar-ka-saamna-kaise/dar-ka-saamna-kaise-video-{vid}/240p.m3u8" \
+  --template "https://video1.acharyaprashant.org/paiso-se-sahi-sambandh/paiso-se-sahi-sambandh-video-{vid}/240p.m3u8" \
   --start 1 --end 20
 
 python downloader.py list \
-  --folder "असली युद्ध अपने विरुद्ध" \
-  --url "https://video1.acharyaprashant.org/courses/2023-12-12/2019-12-21-sangharsh-part1-474b809/240p.m3u8" \
-  --url "https://video1.acharyaprashant.org/courses/2023-12-12/2021-08-18-sangharsh-part2-f1f90c6/240p.m3u8" \
-  --url "https://video1.acharyaprashant.org/courses/2023-12-12/2019-03-24-sangharsh-part3-996a2a6/240p.m3u8"
+  --folder "depression-ke-kaaran-aur-ilaz" \
+  --url "https://video.acharyaprashant.org/depression-ke-kaaran-aur-ilaz/depression-ke-kaaran-aur-ilaz-video-1-v1/240p.m3u8" \
+  --url ""
 
 python downloader.py list --file urls.txt
 
+  # --url "https://video1.acharyaprashant.org/suh23/suh23-cvb-video-1/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/suh23/suh23-cvb-video-2/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/suh23/suh23-cvb-video-3/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/suh23/suh23-cvb-video-4/240p.m3u8"
+  # --url "https://video1.acharyaprashant.org/courses/2021-12-04/by7-video-1-33d79fb/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2021-12-04/by7-video-2-27d0746/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2021-12-04/by7-video-3-77e5f01/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2021-11-29/bgh16-video-1-0164b1a/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2021-11-29/bgh16-video-2-72261a3/240p.m3u8"
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-06/vivekanand2-video-1-4c63980/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-06/vivekanand2-video-2-885b62c/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-06/vivekanand2-video-3-1f67663/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-06/vivekanand2-video-4-6af6e24/240p.m3u8"
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-05/vivekanand1-video-1-d3e01b2/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-05/vivekanand1-video-2-6a12d70/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-05/vivekanand1-video-3-3b54ead/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-05/vivekanand1-video-4-a99e982/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-05/vivekanand1-video-5-cc403cf/240p.m3u8"
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-08/vivekanand4-video-1-a32dce5/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-08/vivekanand4-video-2-3c3c599/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-09/vivekanand4-video-3-906d671/240p.m3u8"
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-08/vivekanand3-video-1-9e649f9/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-08/vivekanand3-video-2-2c4bc21/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-08/vivekanand3-video-3-a29f734/240p.m3u8"
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-15/gs7-video-1-fa18268/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-15/gs7-video-2-4e50cc6/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-15/gs7-video-3-c831317/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2022-01-15/gs7-video-4-456bfd1/240p.m3u8"
 # https://video.acharyaprashant.org/advait-vedanta-english-part-one/advait-vedanta-english-part-one-video-1/240p.m3u8
+# https://video.acharyaprashant.org/advait-vedanta-english-part-two/advait-vedanta-english-part-two-video-1/240p.m3u8
 # https://video.acharyaprashant.org/dar-ka-saamna-kaise/dar-ka-saamna-kaise-video-1/240p.m3u8
-
-#example {vid}
-# python downloader.py template \
+  # --url "https://video1.acharyaprashant.org/courses/2023-12-09/2019-09-13-sangharsh-part14-871f7ab/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2023-12-12/2021-02-13-sangharsh-part15-7f7531f/240p.m3u8" \
+  # --url "https://video1.acharyaprashant.org/courses/2023-12-12/2020-06-12-sangharsh-part16-42d459a/240p.m3u8"
 #   --template "https://video.acharyaprashant.org/love-and-lust/love-and-lust-video-{vid}/240p.m3u8" \
-#   --start 1 --end 20
-
-	
-# python downloader.py template \
 #   --template "https://video.acharyaprashant.org/prem-aur-hawas/prem-aur-hawas-video-{vid}/240p.m3u8" \
-#   --start 1 --end 20
-
-# python downloader.py template \
 #   --template "https://video.acharyaprashant.org/jesus-english/jesus-english-video-{vid}/240p.m3u8" \
-#   --start 1 --end 20 
