3c1f70705d562860cb948380a25f62928d8b87b4
Aakash8494 <72457949+Aakash8494@users.noreply.github.com>
2026-06-06 12:36:19 +0530

    chore : delete old code

--- src/m3u8_downloader_generic/titles_copy_snippet.js
+++
@@ -1,9 +0,0 @@
-let titles = Array.from(document.querySelectorAll('.tutor-course-topic-item-title'))
-    .map((el, index) => `${el.innerText.trim().replace(/[/\\?%*:|"<>]/g, '-')}`);
-
-console.log("✅ Here are your titles:");
-console.log(titles.join('\n'));
-
-// This automatically copies the text to your clipboard!
-copy(titles.join('\n'));
-console.log("📋 ALL TITLES COPIED TO CLIPBOARD! You can paste them in a notepad.");
