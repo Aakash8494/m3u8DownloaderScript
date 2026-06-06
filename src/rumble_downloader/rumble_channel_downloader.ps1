4256f7476e88f652e741418fc744667fa820b4c0
Aakash8494 <72457949+Aakash8494@users.noreply.github.com>
2026-06-06 15:31:20 +0530

    chore : improvements youtube download size

--- src/rumble_downloader/rumble_channel_downloader.ps1
+++ src/rumble_downloader/rumble_channel_downloader.ps1
@@ -1,15 +1,16 @@
 python -m yt_dlp `
     -P "C:\Users\aakas\Downloads" `
-    -f "bestvideo[height<=480][ext=mp4]+bestaudio[ext=m4a]/best[height<=480][ext=mp4]/best" `
+    -f "bestvideo[height<=480]+bestaudio/best[height<=480]/best" `
+    --merge-output-format mkv `
     --playlist-end 20 `
     --concurrent-fragments 4 `
     --download-archive archive.txt `
     -o "%(uploader)s/%(upload_date)s - %(title)s [%(id)s].%(ext)s" `
-    --write-thumbnail `
     --embed-thumbnail `
     --convert-thumbnails jpg `
     --embed-metadata `
-    "https://www.youtube.com/@archer2official"
+    "https://www.youtube.com/@IronWilll2026"
+# "https://www.youtube.com/@archer2official"
 # "https://www.youtube.com/@leonjhendrix"
 
 
